670a37bffd72ca2422c387a34072a04dd0592005
Galaceon <antoniogarcialtr@gmail.com>
2026-04-12 02:12:04 +0200

    displaying expense icons for every category

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,6 +1,8 @@
+import { useMemo } from "react"
 import { formatDate } from "../helpers"
 import type { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
+import { categories } from "../data/cagories"
 
 
 type ExpenseDetailProps = {
@@ -8,13 +10,21 @@ type ExpenseDetailProps = {
 }
 
 export default function ExpenseDetail({expense} : ExpenseDetailProps) {
+
+    const categoryInfo = useMemo(() => categories.filter(cat => cat.id === expense.category)[0], [expense])
+
     return (
-        <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+        <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center first-of-type:mt-5">
             <div>
-                
+                <img 
+                    src={`/icono_${categoryInfo.icon}.svg`}
+                    alt="icono del gasto"
+                    className="w-18"
+                />
             </div>
 
-            <div>
+            <div className="flex-1 space-y-2">
+                <p className="text-sm font-bold uppercase text-slate-500">{categoryInfo.name}</p>
                 <p>{expense.expenseName}</p>
                 <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
             </div>
